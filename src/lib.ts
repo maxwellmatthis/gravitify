26b6aa3c05a8aae9b71e870ed7451822a90901ee
Maxwell Matthis <contact@maxwellmatthis.de>
2024-02-12 15:27:47 +0100

    Add textures

--- src/lib.ts
+++ src/lib.ts
@@ -1,22 +1,29 @@
 import { use, Engine, Render, Runner, Bodies, Composite, Events, Mouse, MouseConstraint } from "matter-js";
 import MatterWrap from "matter-wrap";
-import { toBlob } from 'html-to-image';
+import { toCanvas } from 'html-to-image';
 
-function addChildren(element: HTMLElement, engine: Engine, render: Render) {
-    console.log("elem", element)
+/**
+ * Recursively parses HTML DOM and creates sprites for top level elements.
+ */
+async function addChildren(engine: Engine, render: Render, element: HTMLElement) {
+    console.log("elem", element);
     for (const child of (element.children as any) as HTMLElement[]) {
         if (child.children.length === 0 || child.children.length === 1) {
-            console.log(child)
+            console.log(child, (await toCanvas(child)).toDataURL());
             Composite.add(
                 engine.world,
                 Bodies.rectangle(
                     child.offsetLeft + Math.floor(child.clientWidth / 2),
                     child.offsetTop,
-                    child.clientWidth,
-                    child.clientHeight,
+                    child.offsetWidth,
+                    child.offsetHeight,
                     {
                         render: {
-                            fillStyle: '#ffffff'
+                            sprite: {
+                                texture: (await toCanvas(child)).toDataURL(),
+                                xScale: 0.5,
+                                yScale: 0.5,
+                            },
                         },
                         restitution: 0.6,
                         friction: 0.1
@@ -24,47 +31,23 @@ function addChildren(element: HTMLElement, engine: Engine, render: Render) {
                 )
             );
         }
-        else addChildren(child, engine, render);
+        else addChildren(engine, render, child);
     }
 }
 
-export function gravitify(rootElement: HTMLElement, canvas: HTMLCanvasElement) {
-    const engine = Engine.create();
-    const render = Render.create({
-        // element: rootElement,
-        engine: engine,
-        canvas,
-        options: {
-            width: rootElement.clientWidth,
-            height: rootElement.clientHeight + 200,
-            wireframeBackground: '#fff',
-        }
-    });
-    // function render() {
-    //     const allBodies = engine
-
-    //     const ctx = canvas.getContext("2d");
-
-    //     for (var i = 0; i < bodies.length; i += 1) {
-    //         var vertices = bodies[i].vertices;
-    
-    //         context.moveTo(vertices[0].x, vertices[0].y);
-    
-    //         for (var j = 1; j < vertices.length; j += 1) {
-    //             context.lineTo(vertices[j].x, vertices[j].y);
-    //         }
-    
-    //         context.lineTo(vertices[0].x, vertices[0].y);
-    //     }
-    //     ctx.drawImage(toBlob(document.createElement("p")), )
-    // }
-    const runner = Runner.create();
-
-    // Add elements
-    const floor = Bodies.rectangle(Math.floor(render.options.width / 2), render.options.height, render.options.width, 10, { isStatic: true });
+/**
+ * Adds all the elements and a floor to keep the elements in the canvas to the scene.
+ */
+async function addElementsToScene(engine: Engine, render: Render, rootElement: HTMLElement) {
+    const floor = Bodies.rectangle(Math.floor(render.options.width / 2), render.options.height, render.options.width, 1, { isStatic: true });
     Composite.add(engine.world, floor);
-    addChildren(rootElement, engine, render);
+    await addChildren(engine, render, rootElement);
+}
 
+/**
+ * Adds mouse constraints and interprets mouse events.
+ */
+function addMouseEvents(engine: Engine, render: Render, runner: Runner) {
     // Add Mouse Events
     const mouse = Mouse.create(render.canvas);
     const mouseConstraint = MouseConstraint.create(engine, {
@@ -82,8 +65,13 @@ export function gravitify(rootElement: HTMLElement, canvas: HTMLCanvasElement) {
             // Composite.remove(engine.world, mouseConstraint.body);
         }
     });
+}
 
-    // Keep objects in canvas
+/**
+ * Applies the Matter-Wrap plugin, which keeps the objects in the screen by having objects that
+ * leave the screen enter on the opposite side.
+ */
+function applyMatterWarp(engine: Engine, render: Render) {
     use(MatterWrap);
     Render.lookAt(render, {
         min: { x: 0, y: 0 },
@@ -96,10 +84,29 @@ export function gravitify(rootElement: HTMLElement, canvas: HTMLCanvasElement) {
             max: { x: render.bounds.max.x + 100, y: render.bounds.max.y }
         };
     }
+}
+
+export async function gravitify(rootElement: HTMLElement, canvas: HTMLCanvasElement = document.createElement("canvas")) {
+    const engine = Engine.create();
+    const render = Render.create({
+        engine: engine,
+        canvas,
+        options: {
+            width: rootElement.clientWidth,
+            height: rootElement.clientHeight + 200,
+            wireframes: false,
+            background: "#fff"
+        }
+    });
+    const runner = Runner.create();
+
+    await addElementsToScene(engine, render, rootElement);
+    addMouseEvents(engine, render, runner);
+    applyMatterWarp(engine, render);
 
     // Run engine
     Runner.run(runner, engine);
     Render.run(render);
 
-    return { engine, render };
+    return { engine, render, canvas };
 }
